a53ba9cdab5de7c58a604b241f0a9b2caff50c60
000Lee <1003j6@gmail.com>
2024-11-15 16:41:58 +0900

    up

--- src/features/airSlice.js
+++ src/features/airSlice.js
@@ -1,2 +1,6 @@
 //대기상태 api
 //http://api.openweathermap.org/data/2.5/air_pollution?lat=37.45&lon=126.4161&appid=01252e208ef90ff4d1a0291867d80276
+
+/* 
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { getMovies, getMovieDetails, getMovieCredits, searchMovie } from '../../api/simplewApi' */
--- src/features/currentSlice.js
+++ src/features/currentSlice.js
@@ -1,2 +1,40 @@
-//현재날씨 api
-//https://api.openweathermap.org/data/2.5/weather?q=incheon&appid=01252e208ef90ff4d1a0291867d80276&units=metric&lang=kr
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { getNowWeather } from '../api/simplewApi'
+
+// 현재 날씨 가져오는 비동기 액션
+export const fetchNowWeather = createAsyncThunk('nowweather/fetchNowWeather', async (q) => {
+   const response = await getNowWeather(q)
+   return response.data
+})
+
+const currentSlice = createSlice({
+   name: 'nowweather',
+   initialState: {
+      loading: false, // 로딩 여부 상태
+      nowweather: null, // 현재 날씨 데이터를 저장할 상태
+      error: null, // 에러 메시지 상태
+   },
+   reducers: {
+      resetWeathers(state) {
+         state.nowweather = null // nowweather 상태 초기화
+      },
+   },
+   extraReducers: (builder) => {
+      builder
+         .addCase(fetchNowWeather.pending, (state) => {
+            state.loading = true
+            state.error = null
+         })
+         .addCase(fetchNowWeather.fulfilled, (state, action) => {
+            state.loading = false
+            state.nowweather = action.payload // 응답 데이터를 nowweather 상태에 저장
+         })
+         .addCase(fetchNowWeather.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.error.message
+         })
+   },
+})
+
+export const { resetWeathers } = currentSlice.actions
+export default currentSlice.reducer
